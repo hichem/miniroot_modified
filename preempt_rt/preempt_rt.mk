1ed9a8c9e2ad0fe8de594f16b6a52609632385ac
Hichem Boussetta <boussettahichem@yahoo.fr>
2009-05-25 11:46:03 +0200

    Deleted unused folders : xenomai, rtai, preempt_rt

--- preempt_rt/preempt_rt.mk
+++
@@ -1,9 +0,0 @@
-PREEMPT_RT_PATCH = patch -d $(abspath $(LINUX_SRC_DIR)) -p1 < $(RT_LINUX_PATCH)
-
-.PHONY:preempt_rt_kernel_patch
-#.IGNORE:preempt_rt_kernel_patch
-
-preempt_rt_kernel_patch:
-	@ echo '=== Applying the PREEMPT-RT Patch to the kernel ==='
-	$(PREEMPT_RT_PATCH)
-
